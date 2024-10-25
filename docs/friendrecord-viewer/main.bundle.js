947b144ae6983c6a1fbd78db3b266e272f43648c
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-10-26 02:05:45 +0800

    1

--- docs/friendrecord-viewer/main.bundle.js
+++ docs/friendrecord-viewer/main.bundle.js
@@ -865,12 +865,7 @@
               t = (n - 5e4) / 2 * (e.score - 5e5) / 3e5
             }
             return Math.max(0, t)
-          }(e), e.genre = `${t.genre}`, e.rating = Math.floor(e.rawRating / 100)); 
-          if (e.score === "-") {
-            e.op = 0.00;
-            e.rating = "-";
-          } else { 
-            e.op = function(e) {
+          }(e), e.genre = `${t.genre}`, e.rating = Math.floor(e.rawRating / 100)), e.op = function(e) {
             if (e.score >= 101e4) return Ve(e);
             let t = {
                 AJ: 2e3,
@@ -880,8 +875,8 @@
               n = Math.floor(1e4 * e.const),
               r = e.score < 1007500 ? e.rawRating : n + 2e4 + 3 * (e.score - 1007500);
             return r = e.score >= 975e3 ? 10 * Math.floor(r / 10) : 100 * Math.floor(r / 100), 5 * (r + t)
-          }(e),console.log(e.title,e.op), e.opMax = Ve(e), e.opPercent = 100 * e.op / e.opMax, e.rank = Fe(e.score)
-        }})), n && s.length) {
+          }(e), e.opMax = Ve(e), e.opPercent = 100 * e.op / e.opMax, e.rank = Fe(e.score)
+        })), n && s.length) {
         const e = {};
         s.forEach((t => {
           var n;
