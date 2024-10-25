b9d4b7744a60c8b35d9a98f2ab49a6e88b566dfe
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-10-26 02:32:38 +0800

    0

--- docs/friendrecord-viewer/main.bundle.js
+++ docs/friendrecord-viewer/main.bundle.js
@@ -855,6 +855,7 @@
           let t = o[e.title];
           void 0 === t ? (s.push(e), e.const = -1, e.rating = 0) : (e.const = t[e.difficulty], t.uncertain?.includes(e.difficulty) && (e.constUncertain = !0), e.rawRating = function(e) {
             let t, n = Math.floor(1e4 * e.const);
+            if (e.score === "-") score = 0;
             if (e.score >= 9e5) {
               let t = Be.find((t => e.score >= t.score));
               return Math.max(0, n + t.base + t.ratio * (e.score - t.score))
@@ -867,11 +868,7 @@
             return Math.max(0, t)
           }(e), e.genre = `${t.genre}`, e.rating = Math.floor(e.rawRating / 100));
           
-          if (e.score === "-") {
-            e.score = 0;
-          } else {
-            
-            e.op = function(e) {
+          e.op = function(e) {
             if (e.score >= 101e4) return Ve(e);
             let t = {
                 AJ: 2e3,
@@ -882,7 +879,7 @@
               r = e.score < 1007500 ? e.rawRating : n + 2e4 + 3 * (e.score - 1007500);
             return r = e.score >= 975e3 ? 10 * Math.floor(r / 10) : 100 * Math.floor(r / 100), 5 * (r + t)
           }(e), e.opMax = Ve(e), e.opPercent = 100 * e.op / e.opMax, e.rank = Fe(e.score)
-        }})), n && s.length) {
+        })), n && s.length) {
         const e = {};
         s.forEach((t => {
           var n;
