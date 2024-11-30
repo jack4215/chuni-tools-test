cdcbc3df5a481aa7c7390876d83ed87ef92d253b
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-11-30 15:52:59 +0800

    3

--- docs/record-viewer/main.bundle.js
+++ docs/record-viewer/main.bundle.js
@@ -3056,7 +3056,7 @@
 
     function Pr(e, t, n) {
       let r, o, s, a, i, l, c, d, f;
-      return     console.log("At 值:", At),u(e, At, (e => n(6, a = e))), u(e, At, (e => n(7, i = e))), u(e, At, (e => n(8, l = e))), u(e, Ut, (e => n(3, c = e))), u(e, wt, (e => n(4, d = e))), u(e, $t, (e => n(5, f = e))), e.$$.update = () => {
+      return u(e, At, (e => n(6, a = e))), u(e, At, (e => n(7, i = e))), u(e, At, (e => n(8, l = e))), u(e, Ut, (e => n(3, c = e))), u(e, wt, (e => n(4, d = e))), u(e, $t, (e => n(5, f = e))), e.$$.update = () => {
         256 & e.$$.dirty && n(2, r = l.slice(0, 30).map((e => e.rating))), 128 & e.$$.dirty && n(1, o = i.map((e => e.rating))), 64 & e.$$.dirty && n(0, s = a.map((e => e.rating)))
       }, [s, o, r, c, d, f, a, i, l]
     }
