f6be131be7bf49c12dc626339bc4eb57535a7715
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-12-04 23:13:09 +0800

    1

--- docs/friendrecord-viewer/main.bundle.js
+++ docs/friendrecord-viewer/main.bundle.js
@@ -2661,6 +2661,7 @@
           min: 1,
           step: .1
         };
+        console.log(Ne);
         void 0 !== e[1] && (Ne.high = e[1]), void 0 !== e[2] && (Ne.low = e[2]), f = new Dn({
           props: Ne
         }), Q.push((() => we(f, "high", Me))), Q.push((() => we(f, "low", Ee))), m = new Fn({}), b = new Xn({});
