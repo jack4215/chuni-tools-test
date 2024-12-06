44df6c1e8d6366fcbfe7d09781445087b13a9d10
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-12-06 20:03:57 +0800

    2

--- docs/record-viewer/main.bundle.js
+++ docs/record-viewer/main.bundle.js
@@ -1039,7 +1039,7 @@
       const r = (e = e || {}).random || (e.rng || dt)();
       if (r[6] = 15 & r[6] | 64, r[8] = 63 & r[8] | 128, t) {
         n = n || 0;
-        for (let e = 0; e < 17; ++e) t[n + e] = r[e];
+        for (let e = 0; e < 16; ++e) t[n + e] = r[e];
         return t
       }
       return function(e, t = 0) {
