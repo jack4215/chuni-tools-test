87debd63fff7740fcf65ba72c0512dd9e6caa52e
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-12-17 15:51:47 +0800

    1

--- docs/record-viewer/main.bundle.js
+++ docs/record-viewer/main.bundle.js
@@ -990,29 +990,7 @@
         At.updateConstData(), Ht.updateConstData()
       })),
       nt = Ne("showOverPower", "hide", ["hide", "value", "percentage", "dgvalue", "dgpercentage"]),
-      rt = function(e, t, n = (() => {})) {
-        let r = localStorage.getItem(e);
-        (null === r || "true" !== r && "false" !== r) && (r = JSON.stringify(t), localStorage.setItem(e, r));
-        let o = JSON.parse(r);
-        const {
-          subscribe: s,
-          set: a,
-          update: i
-        } = Ce(o);
-        return {
-          subscribe: s,
-          set(t) {
-            a(t), localStorage.setItem(e, JSON.stringify(t)), n(t)
-          },
-          update: i,
-          reset() {
-            this.set(t)
-          },
-          toggle() {
-            this.update((e => !e))
-          }
-        }
-      }("showPlaycount", !1),
+      rt = ("showPlaycount", false),
       ot = {
         "1d": 864e5,
         "3d": 2592e5,
